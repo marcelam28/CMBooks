6abfea684ec9fa5cefeab39e58d3d4dafba6b75f
CarlaMorar <carla.morar@yahoo.com>
2019-04-04 17:19:38 +0300

    move login function in separate file

--- CMBooks.Web/App_Start/BundleConfig.cs
+++ CMBooks.Web/App_Start/BundleConfig.cs
@@ -8,12 +8,12 @@ namespace CMBooks.Web
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            #region ScriptBundles
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
-
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -22,19 +22,23 @@ namespace CMBooks.Web
                         "~/Scripts/AjaxHelper.js"));
             bundles.Add(new ScriptBundle("~/bundles/toastr").Include(
                         "~/Scripts/toastr.js"));
-
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                        "~/Scripts/bootstrap.js"));
             bundles.Add(new ScriptBundle("~/bundles/login").Include(
-                      "~/Scripts/login.js"));
+                        "~/Scripts/login.js"));
+            bundles.Add(new ScriptBundle("~/bundles/login-f").Include(
+                        "~/Scripts/login-f.js"));
+            #endregion
 
+            #region StyleBundles
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
-            bundles.Add(new StyleBundle("~/Content/login").Include
-                ("~/Content/login.css"));
-            bundles.Add(new StyleBundle("~/Content/toastr").Include
-                ("~/Content/toastr.css"));
+                       "~/Content/bootstrap.css",
+                       "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/login").Include(
+                       "~/Content/login.css"));
+            bundles.Add(new StyleBundle("~/Content/toastr").Include(
+                       "~/Content/toastr.css"));
+            #endregion
 
         }
     }
